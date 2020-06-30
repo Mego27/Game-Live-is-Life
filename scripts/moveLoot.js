289f68c8dd4097f0d8cc60cede42d6aa609eafa7
Mego <57959735+Mego27@users.noreply.github.com>
2020-06-30 12:35:36 +0300

    add in html/css radiobuttons of choice hero

--- scripts/moveLoot.js
+++ scripts/moveLoot.js
@@ -1,7 +1,6 @@
-function moveLoot(elem, y, speed) {
+export default function moveLoot(elem, y, speed) {
     elem.link.style.visibility = 'visible';
     elem.link.style.transition = `transform ${speed}s linear`;
     //elem.link.style.transition = `transform 3s linear`;
     elem.link.style.transform = `translate(${elem.x}px, ${y}px) rotate(${Math.floor(Math.random() * (1800+1800)) - 1800}deg)`;
 }
-export {moveLoot}
